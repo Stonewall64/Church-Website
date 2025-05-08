52e52b30fb7cca4b410567833a4eca32d97fff21
Stonewall64 <78286785+Stonewall64@users.noreply.github.com>
2025-05-08 15:31:41 -0400

    Added JS
    Added scripting info

---
+++ Scripts/scripts.js
@@ -0,0 +1,16 @@
+
+function addNavBar() {
+
+    let tempData = document.getElementsByTagName("template");
+    document.body.appendChild(tempData.content);
+
+}
+addNavBar;
+console.log("hello");
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+
+
+
+
