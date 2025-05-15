b78bdd1fb58af9c00a2707d5e99fb223c7580bda
Stonewall64 <78286785+Stonewall64@users.noreply.github.com>
2025-05-15 14:29:58 -0400

    Appearance
    Began the first (of many) CSS and design updates to make this template look more visually appealing. I also tried to work on a script to call another HTML page for the nav bar, but that did not work. That is the new file "testFile.html"

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,16 +1,27 @@
-
-function addNavBar() {
-
-    let tempData = document.getElementsByTagName("template");
-    document.body.appendChild(tempData.content);
-
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("navBar-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("navBar-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
 }
-addNavBar;
-console.log("hello");
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-
-
-
-
-
